1fad193b2e5d736268c1fac8d73071ce06b7f7bf
정상원 <topcircle@hanyang.ac.kr>
2024-03-02 13:20:54 +0900

    init

---
+++ src/main/java/sangwon/wead/WeadApplication.java
@@ -0,0 +1,13 @@
+package sangwon.wead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sangwon/wead/WeadApplicationTests.java
@@ -0,0 +1,13 @@
+package sangwon.wead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
